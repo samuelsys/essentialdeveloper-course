a0c8861b3d0daa4fe1a4d8380d45dceac8358d6e
Samuel Silva <samuelfurtado1@gmail.com>
2020-03-19 16:26:32 -0300

    Create Feed Item

---
+++ student-classes/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Samuel Furtado on 19/03/20.
+//  Copyright © 2020 EssentialDeveloper. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
